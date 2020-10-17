0a9a307961242cd98c01a209a5827455e09b5934
SOG-web <raheemolalekanusman@gmail.com>
2020-10-17 10:21:02 +0100

    testing

--- GOUBEB/src/app/app-routing.module.ts
+++ GOUBEB/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [{ path: "login", component: HomeComponent }];
+const routes: Routes = [
+  { path: "login", component: HomeComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
